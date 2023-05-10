185b282835e10faeac5407b636b37774a724ac0e
Dani <danielscout9@gmail.com>
2023-05-10 17:32:50 -0500

    Added modal to edit information

--- crud.js
+++ crud.js
@@ -16,25 +16,37 @@ function onSubmit(e) {
 const myButton = document.getElementById('buttonSubmit');
 myButton.addEventListener('click', () => onSubmit(event))
 
+/* Function to edit the content within the modal */
+function editContent(id) {
+    const info = JSON.parse(localStorage.getItem("Info"))
+    console.log(info[id])
+    const { name, email } = info[id] //Deconstruct JS//
+    const emailInput = document.getElementById('InputEditemail');
+    const nameInput = document.getElementById('InputEditname');
+    emailInput.value = email
+    nameInput.value = name
+
+}
+
 function printInfo() {
     const info = JSON.parse(localStorage.getItem("Info"))
-        console.log(info)
+    console.log(info)
     const box = document.getElementById('box')
-    if ( info ){
-        box.innerHTML= "";
+    if (info) {
+        box.innerHTML = "";
 
-        info.forEach(function (item) {
+        info.forEach(function (item, index) {
             let content = `<tr>
-            <th scope="row">1</th>
+            <th scope="row">${index}</th>
             <td>${item.email}</td>
             <td>${item.name}</td>
-            <td><button type="button" class="btn btn-primary">Edit</button>
+            <td><button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#editModal" onclick="editContent(${index})">Edit</button>
                 <button type="button" class="btn btn-danger">Delete</button>
             </td>
             </tr>`;
             box.insertAdjacentHTML("afterbegin", content);
-    });
-          
+        });
+
     } else {
 
 
